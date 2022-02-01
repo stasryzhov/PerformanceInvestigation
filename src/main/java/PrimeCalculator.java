012fc4053580c94cde4bc57aaa28dc44ac764506
ryzhovss <60649800+stasryzhov@users.noreply.github.com>
2022-02-01 04:36:40 +0100

    Revert "Use sieve-of-eratosthenes algorithm"
    This reverts commit d654b265

--- src/main/java/PrimeCalculator.java
+++ src/main/java/PrimeCalculator.java
@@ -1,20 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class PrimeCalculator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (Integer prime : getPrimes(Integer.parseInt(args[0]))) {
             System.out.print(prime + "\n");
         }
     }
 
-    private static List<Integer> getPrimes(int maxPrime) {
-        if (maxPrime <= 1) {
-            throw new IllegalArgumentException("Max prime number must be > 1");
+    private static List<Integer> getPrimes(int maxPrime) throws InterruptedException {
+        boolean[] isPrimeMarks = new boolean[maxPrime];
+        CountDownLatch latch = new CountDownLatch(maxPrime - 1);
+        ExecutorService executors = Executors.newFixedThreadPool(50);
+        for (int i = 2; i <= maxPrime; i++) {
+            int candidate = i;
+            executors.submit(() -> {
+                if (isPrime(candidate)) {
+                    isPrimeMarks[candidate] = true;
+                }
+                latch.countDown();
+            });
         }
-        boolean[] isPrimeMarks = new boolean[maxPrime + 1];
-        fillWithInitialValues(isPrimeMarks);
-        applySieveOfEratosthenes(isPrimeMarks);
+        latch.await();
+        executors.shutdownNow();
         return collectPrimes(isPrimeMarks);
     }
 
@@ -28,19 +39,12 @@ public class PrimeCalculator {
         return primeNumbers;
     }
 
-    private static void applySieveOfEratosthenes(boolean[] isPrimeMarks) {
-        for (int factor = 2; factor * factor < isPrimeMarks.length; factor++) {
-            if (isPrimeMarks[factor]) {
-                for (int j = factor; factor * j < isPrimeMarks.length; j++) {
-                    isPrimeMarks[factor * j] = false;
-                }
+    private static boolean isPrime(int candidate) {
+        for (int i = 2; i * i <= candidate; i++) {
+            if (candidate % i == 0) {
+                return false;
             }
         }
-    }
-
-    private static void fillWithInitialValues(boolean[] isPrimeMarks) {
-        for (int i = 2; i < isPrimeMarks.length; i++) {
-            isPrimeMarks[i] = true;
-        }
+        return true;
     }
 }
