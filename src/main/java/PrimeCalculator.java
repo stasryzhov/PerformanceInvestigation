86f16f452406cc8086922fec6f65d45176c51be3
ryzhovss <60649800+stasryzhov@users.noreply.github.com>
2022-02-01 04:00:05 +0100

    Mark primes via boolean array

--- src/main/java/PrimeCalculator.java
+++ src/main/java/PrimeCalculator.java
@@ -1,12 +1,8 @@
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PrimeCalculator {
     public static void main(String[] args) throws InterruptedException {
@@ -16,36 +12,36 @@ public class PrimeCalculator {
     }
 
     private static List<Integer> getPrimes(int maxPrime) throws InterruptedException {
-        List<Integer> numbers = Stream.generate(new Supplier<Integer>() {
-            int i = 2;
-
-            @Override
-            public Integer get() {
-                return i++;
-            }
-        }).limit(maxPrime).collect(Collectors.toList());
-        List<Integer> primeNumbers = Collections.synchronizedList(new LinkedList<>());
+        boolean[] numbers = new boolean[maxPrime];
         CountDownLatch latch = new CountDownLatch(maxPrime - 1);
         ExecutorService executors = Executors.newFixedThreadPool(50);
         for (int i = 2; i <= maxPrime; i++) {
             int candidate = i;
             executors.submit(() -> {
-                if (isPrime(numbers, candidate)) {
-                    primeNumbers.add(candidate);
+                if (isPrime(candidate)) {
+                    numbers[candidate] = true;
                 }
                 latch.countDown();
             });
         }
         latch.await();
         executors.shutdownNow();
+        return collectPrimes(numbers);
+    }
 
-        Collections.sort(primeNumbers);
+    private static List<Integer> collectPrimes(boolean[] numbers) {
+        List<Integer> primeNumbers = new ArrayList<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i]) {
+                primeNumbers.add(i);
+            }
+        }
         return primeNumbers;
     }
 
-    private static boolean isPrime(List<Integer> numbers, int candidate) {
-        for (Integer j : numbers.subList(0, (int) Math.sqrt(candidate - 2))) {
-            if (candidate % j == 0) {
+    private static boolean isPrime(int candidate) {
+        for (int i = 2; i * i <= candidate; i++) {
+            if (candidate % i == 0) {
                 return false;
             }
         }
