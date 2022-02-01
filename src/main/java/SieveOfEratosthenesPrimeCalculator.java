c05fed62ff4ea30a830121c8a595ad0211f1d802
ryzhovss <60649800+stasryzhov@users.noreply.github.com>
2022-02-01 04:41:49 +0100

    Add separate class for sieve-of-eratosthenes solution

---
+++ src/main/java/SieveOfEratosthenesPrimeCalculator.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SieveOfEratosthenesPrimeCalculator {
+
+
+    public static void main(String[] args) {
+        for (Integer prime : getPrimes(Integer.parseInt(args[0]))) {
+            System.out.print(prime + "\n");
+        }
+    }
+
+    private static List<Integer> getPrimes(int maxPrime) {
+        boolean[] isPrimeMarks = new boolean[maxPrime + 1];
+        fillWithInitialValues(isPrimeMarks);
+        applySieveOfEratosthenes(isPrimeMarks);
+        return collectPrimes(isPrimeMarks);
+    }
+
+    private static List<Integer> collectPrimes(boolean[] isPrimeMarks) {
+        List<Integer> primeNumbers = new ArrayList<>();
+        for (int i = 0; i < isPrimeMarks.length; i++) {
+            if (isPrimeMarks[i]) {
+                primeNumbers.add(i);
+            }
+        }
+        return primeNumbers;
+    }
+
+    private static void applySieveOfEratosthenes(boolean[] isPrimeMarks) {
+        for (int factor = 2; factor * factor < isPrimeMarks.length; factor++) {
+            if (isPrimeMarks[factor]) {
+                for (int j = factor; factor * j < isPrimeMarks.length; j++) {
+                    isPrimeMarks[factor * j] = false;
+                }
+            }
+        }
+    }
+
+    private static void fillWithInitialValues(boolean[] isPrimeMarks) {
+        for (int i = 2; i < isPrimeMarks.length; i++) {
+            isPrimeMarks[i] = true;
+        }
+    }
+}
+
